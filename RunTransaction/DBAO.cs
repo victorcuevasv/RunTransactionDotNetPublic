7d9244c730ab133b7626bdd4003b1f3bf8d20564
vcv <vcv>
2016-04-24 16:21:23 -0500

    First commit.

---
+++ RunTransaction/DBAO.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using Npgsql;
+
+namespace RunTransaction
+{
+	public class DBAO
+	{
+
+		private string sqlDriver;
+		private string sqlUrl;
+
+		NpgsqlConnection conn;
+
+		public DBAO ()
+		{
+			sqlDriver = ConfigurationManager.AppSettings ["flightservice.driver"];
+			sqlUrl = ConfigurationManager.AppSettings ["flightservice.url"];
+		}
+
+		public void OpenConnection()
+		{
+			try {
+				conn = new NpgsqlConnection(sqlUrl);
+				conn.Open();
+			}
+			catch(Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+		public void CloseConnection()
+		{
+			conn.Close();
+		}
+
+		public void ExecuteSelectStatement (string query)
+		{
+			try {
+
+				NpgsqlCommand stmt = new NpgsqlCommand (query, conn);
+				NpgsqlDataReader dr = stmt.ExecuteReader ();
+
+				Console.WriteLine ("");
+				for (int i = 0; i < dr.FieldCount; i++) {
+					if (i > 0)
+						Console.Write (",  ");
+					String columnName = dr.GetName (i);
+					Console.Write (columnName);
+				}
+				Console.WriteLine ("");
+
+				while (dr.Read ()) {
+					for (int i = 0; i < dr.FieldCount; i++) {
+						if (i > 0)
+							Console.Write (",  ");
+						String columnValue = dr.GetString (i);
+						Console.Write (columnValue);
+					}
+					Console.WriteLine ("");
+				}
+				dr.Close();
+			} catch (Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+		public void ExecuteUpdateStatement(String sqlStmtStr) {
+			try {
+				NpgsqlCommand stmt = new NpgsqlCommand(sqlStmtStr, conn);
+				Int32 modified = stmt.ExecuteNonQuery();
+				Console.WriteLine(modified + " rows modified");
+			}
+			catch(Exception e) {
+				Console.WriteLine(e.Message);
+			}
+		}
+
+		public void BeginTransaction(string isolationLevel) {
+			try {
+				NpgsqlCommand stmt = new NpgsqlCommand("BEGIN ISOLATION LEVEL " + isolationLevel + ";", conn);
+				stmt.ExecuteNonQuery();
+				Console.WriteLine("begin successful");
+			}
+			catch(Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+
+		public void CommitTransaction() {
+			try {
+				NpgsqlCommand stmt = new NpgsqlCommand("COMMIT;", conn);
+				stmt.ExecuteNonQuery();
+				Console.WriteLine("commit successful");
+			}
+			catch(Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+
+		public void RollbackTransaction() {
+			try {
+				NpgsqlCommand stmt = new NpgsqlCommand("ROLLBACK;", conn);
+				stmt.ExecuteNonQuery();
+				Console.WriteLine("rollback successful");
+			}
+			catch(Exception e) {
+				Console.WriteLine (e.Message);
+			}
+		}
+
+
+	}
+}
+
---
+++ RunTransaction/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace RunTransaction
+{
+	class MainClass
+	{
+
+		private const string DBCONFIG_FILENAME = "dbconn.properties";
+
+		private string[] isolationLevels= {"READ UNCOMMITTED", "READ COMMITTED", 
+			"REPEATABLE READ", "SERIALIZABLE"};
+
+		private DBAO dbao;
+
+		public MainClass()
+		{
+			/* prepare the database connection stuff */
+			dbao = new DBAO();
+			dbao.OpenConnection();
+		}
+
+		public void Usage()
+		{
+			/* prints the choices for commands and parameters */
+			Console.WriteLine();
+			Console.WriteLine(" *** Please enter one of the following commands *** ");
+			Console.WriteLine("> begin transaction");
+			Console.WriteLine("> execute sql statement");
+			Console.WriteLine("> commit transaction");
+			Console.WriteLine("> rollback transaction");
+			Console.WriteLine("> quit");
+		}
+
+		public string[] Tokenize(string command)
+		{
+			string pat = "\"([^\"]*)\"|(\\S+)";
+			Regex r = new Regex(pat);
+			// Match the regular expression pattern against a text string.
+			Match m = r.Match(command);
+			List<string> tokens = new List<string> ();
+			while (m.Success) 
+			{
+				for (int i = 1; i <= 2; i++) 
+				{
+					Group g = m.Groups[i];
+					if (g.Length > 0)
+					{
+						tokens.Add (g.Value);
+					}
+				}
+				m = m.NextMatch();
+			}
+			return tokens.ToArray();
+		}
+
+		public void Menu ()
+		{
+			string command = null;
+			while (true) {
+				Usage ();
+				Console.Write ("> ");
+				command = Console.ReadLine ();
+				string[] tokens = Tokenize (command.Trim ());
+				if (tokens.Length == 0) {
+					Console.WriteLine ("Please enter a command");
+					continue; // back to top of loop
+				}
+				if (tokens [0].Equals ("begin")) {
+					int retVal = BeginTransactionOption();
+					if (retVal == -1)
+						continue;
+				} else if (tokens [0].Equals ("execute")) {
+					Console.WriteLine ();
+					Console.WriteLine (" *** Please enter the SQL statement *** ");
+					string sqlStmtStr = Console.ReadLine ();
+					if (sqlStmtStr.Trim ().ToUpper ().StartsWith ("SELECT")) {
+						dbao.ExecuteSelectStatement(sqlStmtStr);
+					} else {
+						dbao.ExecuteUpdateStatement(sqlStmtStr);
+					}
+				} else if (tokens [0].Equals ("commit")) {
+					dbao.CommitTransaction();
+				} else if (tokens [0].Equals ("rollback")) {
+					dbao.RollbackTransaction();
+				} else if (tokens [0].Equals ("quit")) {
+					Environment.Exit (0);
+				} else {
+					Console.WriteLine ("Error: unrecognized command '" + tokens [0] + "'");
+				}
+			} //end while
+		}
+
+		private int BeginTransactionOption() {
+			int retVal = -1;
+			Console.WriteLine();
+			Console.WriteLine(" *** Please select a transaction isolation level *** ");
+			Console.WriteLine("> [1] READ UNCOMMITTED");
+			Console.WriteLine("> [2] READ COMMITTED");
+			Console.WriteLine("> [3] REPEATABLE READ");
+			Console.WriteLine("> [4] SERIALIZABLE");
+			try {
+				String optionStr = Console.ReadLine();
+				int option = Int32.Parse(optionStr);
+				if (option >= 1 && option <= 4) {
+					dbao.BeginTransaction(isolationLevels[option - 1]);
+					retVal = 1;
+				}
+				else {
+					Console.WriteLine("Error: invalid option");
+					retVal = -1;
+				}
+			}
+			catch (FormatException e) {
+				Console.WriteLine("Error: invalid option");
+				retVal = -1;
+			}
+			return retVal;
+		}
+
+		public void closeConnection()
+		{
+			dbao.CloseConnection();
+		}
+
+		public static void Main (string[] args)
+		{
+			MainClass app = new MainClass ();
+			app.Menu ();
+			Console.ReadLine ();
+		}
+
+	}
+}
